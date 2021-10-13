5a353ae5531413947a6dcefe10251198c81d0b5c
Vandankumar Patel <pate0786@algonquinlive.com>
2021-10-12 20:02:55 -0400

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Lab3.Models;
+
+namespace Lab3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Razor() => View();
+
+        [HttpPost]
+        public IActionResult Count()
+        {
+            
+            return View();
+        }
+
+        public IActionResult CreatePerson() => View();
+
+        [HttpPost]
+        public IActionResult DisplayPerson(Person person)
+        {
+            return View(person);
+        }
+        public IActionResult Error()
+        {
+            return View();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/Person.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Lab3.Models
+{
+    public class Person
+    {
+        [Required]
+        [StringLength(50)]
+        [BindProperty(Name = "firstName")]
+        public String FirstName { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [BindProperty(Name = "lastName")]
+        public String LastName { get; set; }
+
+        [Required]
+        [BindProperty(Name = "age")]
+        public int Age { get; set; }
+
+        [Required]
+        [BindProperty(Name = "email")]
+        [StringLength(255)]
+        public String EmailAddress { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        [BindProperty(Name = "pwd")]
+        public String Password { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        [BindProperty(Name = "description")]
+        public String Description { get; set; }
+    }
+}
